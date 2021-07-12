13efd5b6b87d6d1ea96106ef2404edb0699493e3
GambarovaTamara <gambarova.tamara@gmail.com>
2021-07-12 21:02:02 +0300

    Add initial xdr generated code

--- lib/utils/xdr_primitives.dart
+++ lib/utils/xdr_primitives.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:dart_wallet/xdr/utils/xdr_encodable.dart';
 import 'package:stellar/stellar.dart';
 
 int intFromXdr(XdrDataInputStream stream) {
@@ -16,4 +17,30 @@ bool boolFromXdr(XdrDataInputStream stream) {
 
 Uint8List opaqueFromXdr(XdrDataInputStream stream) {
   return Uint8List.fromList(stream.readIntArray());
+}
+
+abstract class XdrFixedByteArray extends XdrEncodable {
+  abstract int size;
+  late Uint8List wrapped;
+
+  set aWrapped(Uint8List value) {
+    if (value.length == wrapped.length) {
+      wrapped = value;
+    } else if (value.length > wrapped.length) {
+      wrapped =
+          Uint8List.fromList(value.getRange(0, wrapped.length - 1).toList());
+    } else if (value.length < wrapped.length) {
+      wrapped = Uint8List(wrapped.length);
+      for (int i = 0; i < value.length; i++) {
+        wrapped[i] = value[i];
+      }
+    }
+  }
+
+  XdrFixedByteArray(this.wrapped);
+
+  @override
+  toXdr(XdrDataOutputStream stream) {
+    stream.writeIntArray(wrapped);
+  }
 }
---
+++ lib/xdr/utils/dependencies.dart
@@ -0,0 +1,8 @@
+export 'package:dart_wallet/xdr/utils/xdr_decodable.dart';
+export 'package:dart_wallet/xdr/utils/xdr_encodable.dart';
+export 'package:dart_wallet/extensions/xdr_primitives.dart';
+export 'dart:typed_data';
+export 'package:dart_wallet/utils/xdr_primitives.dart';
+export 'package:dart_wallet/xdr/xdr_byte_array_fixed32.dart';
+export 'package:dart_wallet/xdr/xdr_byte_array_fixed16.dart';
+export 'package:dart_wallet/xdr/xdr_byte_array_fixed4.dart';
